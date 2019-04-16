7342e1eef7ff839eb479f81b36d1e890bd19d67e
Michelle Xu <z5115058@unsw.edu.au>
2019-04-16 21:41:27 +1000

    css, controllers added for each page

--- src/pkg2605assignment/NeedHelpPageController.java
+++ src/pkg2605assignment/NeedHelpPageController.java
@@ -7,26 +7,18 @@ package pkg2605assignment;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
- * @author elizabethlin
+ * @author michellexu
  */
-public class FXMLDocumentController implements Initializable {
-    
-    private Label label;
-    @FXML
-    private Label label2;
-    
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+public class NeedHelpPageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
