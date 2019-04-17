1d23bc519f4f654c740d8373df0b0bda50e86639
Liz <lizlinn97@gmail.com>
2019-04-17 18:34:55 +1000

    m

--- src/pkg2605assignment/MindfulnessController.java
+++ src/pkg2605assignment/MindfulnessController.java
@@ -6,6 +6,11 @@
 package pkg2605assignment;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,7 +24,7 @@ import javafx.scene.chart.NumberAxis;
  * @author elizabethlin
  */
 public class MindfulnessController implements Initializable {
-    
+
     @FXML
     private BarChart<?, ?> mindfulnessweek;
 
@@ -29,13 +34,33 @@ public class MindfulnessController implements Initializable {
     @FXML
     private NumberAxis weekminutes;
 
-
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+
+    }
+
+    public static void printBarchart() throws SQLException {
+        //create connection
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:assignmentdata.db");
+       
+        //create statement
+        Statement st = conn.createStatement();
+        
+        System.out.println("Connected");
+        
+        String selectQuery = "SELECT ID, ActivityDay, MindfulMinutes FROM Mental wellbeing;";
+
+        ResultSet rs = st.executeQuery(selectQuery);
+        
+        mindfulnessweek.
+        
+        st.close();
+        conn.close();
+
+    }
+
 }
