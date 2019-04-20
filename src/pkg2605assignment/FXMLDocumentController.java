be3a22bab2e49104a3756d2857845e717f89de49
Geneve Estrada <geneve.estrada@outlook.com>
2019-04-21 01:40:27 +1000

    Login done

--- src/pkg2605assignment/FXMLDocumentController.java
+++ src/pkg2605assignment/FXMLDocumentController.java
@@ -1,14 +1,84 @@
 package pkg2605assignment;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class FXMLDocumentController implements Initializable {
 
-   
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Label result;
+    @FXML
+    private AnchorPane holderPane;
+    @FXML
+    private CheckBox rememberMe;
+    @FXML
+    private Button btnSignIn;
+    @FXML
+    private Button btnSignUp;
+    @FXML
+    private Label welcome1;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //Load all fxmls in a cache
+
+    }
+
+    @FXML
+    private void signIn(MouseEvent event) throws SQLException, IOException {
+        String user, pass;
+
+        user = username.getText();
+        pass = password.getText();
+
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:fitnessdata.db");
+
+        Statement st = conn.createStatement();
+        String loginQuery = "SELECT * FROM User WHERE username = '" + user + "' and password = '" + pass + "';";
+
+        try {
+            ResultSet rs = st.executeQuery(loginQuery);
+            if (rs.next()) {
+                result.setText("");
+                AnchorPane pane = FXMLLoader.load(getClass().getResource("Homepage.fxml"));
+                holderPane.getChildren().setAll(pane);
+
+            } else {
+                result.setText("Your Sign On details are incorrect. Please check your details and try again.");
+
+            }
+        } catch (Exception e) {
+            result.setText("Your Sign On details are incorrect. Please check your details and try again.");
+        }
+    }
+
+    @FXML
+    private void signUp(MouseEvent event) {
+        
     }
 }
